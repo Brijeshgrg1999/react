336ed0c9deaec5a5e8855fc07bc88a9f532bb979
Brijeshgrg1999 <brijeshgurung187@gmail.com>
2023-02-13 09:22:57 +0800

    styling with tailwind

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   console.log(" list the employee")
   const showEmoployees = true ; 
   return (
-    <div className="App bg-red-300">
+    <div className="App ">
      
       {showEmoployees ? (
         <>
@@ -17,12 +17,22 @@ function App() {
           console.log(e.target.value);
           setRoles(e.target.value); 
         }} />
-      <Employee name="brijesh" role="intern"/>
-      <Employee name = "era" role={role}/>
-      <Employee name = "ram" />
 
+              <div className='flex flex-wrap justify-center'>
+              <Employee name="Briejsh" role="intern" img="https://images.pexels.com/photos/4342401/pexels-photo-4342401.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/>
+              <Employee name = "Era" role={role}/>
+              <Employee name = "Rista" />
+              <Employee name = "Rista" />
+              <Employee name = "Rista" />
+              <Employee name = "Rista" />
+
+              </div>
+
+
+ 
       </>
-      ): <p> no employee</p>}
+      ): <p> no employee</p>
+      }
     </div>
   );
 }
